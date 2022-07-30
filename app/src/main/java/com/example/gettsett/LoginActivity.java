60db68b7087480b50e5de9e56212cd3029fab117
Veer Sharma <sharma.rvasv.4@gmail.com>
2022-07-30 12:43:21 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/gettsett/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.gettsett;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    ImageView backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        backButton = findViewById(R.id.backBtn);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
